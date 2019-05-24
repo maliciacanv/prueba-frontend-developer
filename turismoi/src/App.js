3c4384df88f539334642e73fe1569fd1bf36fbd3
maliciacanv <cantenovictoriomaruja@gmail.com>
2019-05-24 14:11:07 -0500

    agregando prepocesador sass al proyecto

--- turismoi/src/App.js
+++ turismoi/src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <>
+      <div className="search"> 
+      <h1>soy un buscador</h1>
+      </div>
+      <div className="order">
+        <select>Order</select>
+      </div> 
+      <div className="cards">
+        <img src="foto" alt="foto" />
+
+      </div>
+      </>
+    );
+  }
 }
 
 export default App;
