6de0f348d09f88b40d6ca5c725242cb5dc46185b
maliciacanv <cantenovictoriomaruja@gmail.com>
2019-05-24 15:08:44 -0500

    maquetado de la interfaz listo

--- turismoi/src/App.js
+++ turismoi/src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
 
@@ -7,14 +7,29 @@ class App extends Component {
     return (
       <>
       <div className="search"> 
-      <h1>soy un buscador</h1>
+        <form role="search">
+          <div class="col-3">
+            <input class="form-control" type="search"
+              placeholder="Buscar en el sitio..." />
+            <button type="button" class="btn btn-primary">Buscar</button>
+          </div>
+        </form>
       </div>
       <div className="order">
-        <select>Order</select>
+        <select name="Order" class="custom-select col-md-1 mb-1">
+          <option class="d-none" value="">Order</option> 
+          <option value="precio">Precio</option> 
+          <option value="dias">Días</option>
+        </select>
       </div> 
       <div className="cards">
-        <img src="foto" alt="foto" />
-
+        <div class="card" style={{width:"18rem"}}>
+          <img src="..." class="card-img-top" alt="..." />
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+        </div>
       </div>
       </>
     );
