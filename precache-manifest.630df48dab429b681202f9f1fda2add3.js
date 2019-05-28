676dec59903cfbe6a349f0fa2b6602d5dad03a07
maliciacanv <cantenovictoriomaruja@gmail.com>
2019-05-28 11:45:41 -0500

    Updates

---
+++ precache-manifest.630df48dab429b681202f9f1fda2add3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a495b8bb1d9e763399e3a4202b8d5dd6",
+    "url": "/prueba-frontend-developer/index.html"
+  },
+  {
+    "revision": "4a0fced60c1e12fca21b",
+    "url": "/prueba-frontend-developer/static/css/main.2e960c45.chunk.css"
+  },
+  {
+    "revision": "31a31ed23b51c02febb9",
+    "url": "/prueba-frontend-developer/static/js/2.c8ac3f43.chunk.js"
+  },
+  {
+    "revision": "4a0fced60c1e12fca21b",
+    "url": "/prueba-frontend-developer/static/js/main.e8ac059f.chunk.js"
+  },
+  {
+    "revision": "a76fca978287d841b6a4",
+    "url": "/prueba-frontend-developer/static/js/runtime~main.2523e7f7.js"
+  }
+]);
